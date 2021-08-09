5e7f4738b7674d5a942f549c0b3af2086ff7e0bc
Mehrdad <Mehrdad.HMT@Gmail.com>
2021-08-09 22:35:47 +0430

    create hello app to play with session and coockies!

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'polls.apps.PollsConfig',
+    'hello.apps.HelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),    
+    path('polls/', include('polls.urls')),
+    path('hello/', include('hello.urls')),    
 ]
