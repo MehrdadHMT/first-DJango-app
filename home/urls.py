57a8b3b3d2dab5dc1300cc9e4b7ce622c24cd93b
Mehrdad <Mehrdad.HMT@Gmail.com>
2021-08-09 22:09:42 +0430

    simple home page

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views
+
+
+app_name = 'home'
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home/main.html')),
+]
