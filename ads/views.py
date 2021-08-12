ae9252e19d0d7c7b0f3f9ff5eddb01ed208a2ad8
Mehrdad <Mehrdad.HMT@Gmail.com>
2021-08-12 23:16:55 +0430

    Create Ads app

---
+++ ads/views.py
@@ -0,0 +1,26 @@
+from .models import Ad
+from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+
+
+class AdListView(OwnerListView):
+    model = Ad
+    # By convention:
+    # template_name = "myarts/ad_list.html"
+
+
+class AdDetailView(OwnerDetailView):
+    model = Ad
+
+
+class AdCreateView(OwnerCreateView):
+    model = Ad
+    fields = ['title', 'price', 'text']
+
+
+class AdUpdateView(OwnerUpdateView):
+    model = Ad
+    fields = ['title', 'price', 'text']
+
+
+class AdDeleteView(OwnerDeleteView):
+    model = Ad
--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from django.views.generic import TemplateView
 from . import views
+from django.contrib.auth import views as auth_views
 
-
-app_name = 'home'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home/main.html')),
+    path('', views.HomeView.as_view()),
 ]
